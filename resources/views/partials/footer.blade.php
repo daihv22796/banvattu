42e9596262e79f200083d404b8430ad334f238aa
daihv <daihv.dev@gmail.com>
2021-10-26 11:48:35 +0700

    first commit

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,9 @@
+<!-- BEGIN FOOTER -->
+<div class="page-footer">
+    <div class="page-footer-inner pull-right">Phần mềm quản lý Hợp đồng - Ban Vật tư - Nhựa Tiền Phong - v1.0.0-beta</div>
+    
+    <div class="scroll-to-top">
+        <i class="icon-arrow-up"></i>
+    </div>
+</div>
+<!-- END FOOTER -->
